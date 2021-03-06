5f73a685dfa1f0ba7e1a260818233218a3da6004
Shreejoy Dash <shreejoy@pixysos.com>
2021-03-06 22:00:24 +0530

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -11,7 +11,7 @@ python3 generate_api.py
 
 cd api
 git add .
-CHANGED =$(git diff --numstat --cached | wc -l)
+CHANGED=$(git diff --numstat --cached | wc -l)
 [[ $CHANGED -eq 0 ]] && exit 0
 git commit -m "generate_api: $(date)"
 git push https://"${GUSER}":"${GPASSWORD}"@github.com/"${GITHUB_REPOSITORY}" api
