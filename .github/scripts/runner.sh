fa046421edd5970ab17d6f2b52b16a31bce218e5
Shreejoy Dash <shreejoy@pixysos.com>
2021-03-06 22:03:14 +0530

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-cd "$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 rm -rf master api
-
-git config --global user.email "$GEMAIL"
-git config --global user.name "$GUSER"
-
+git config --global user.email "${GEMAIL}"
+git config --global user.name "${GUSER}"
 git clone https://github.com/"${GITHUB_REPOSITORY}" -b master master
 git clone https://github.com/"${GITHUB_REPOSITORY}" -b api api
 python3 generate_api.py
@@ -16,4 +14,4 @@ git add .
 CHANGED=$(git diff --numstat --cached | wc -l)
 [[ $CHANGED -eq 0 ]] && exit 0
 git commit -m "generate_api: $(date)"
-git push https://"${GUSER}":"${GPASSWORD}"@github.com/"${GITHUB_REPOSITORY}" api
+git push https://"${GUSER}":"${GPASSWORD}"@github.com/"${GITHUB_REPOSITORY}" api 
