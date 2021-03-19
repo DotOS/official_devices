089ff6ac06a1aca26a9a307f18010c9e15c8ae58
Mohan C M <mohanmanjappa@gmail.com>
2021-03-19 04:24:30 +0000

    update git repos path

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -5,8 +5,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf master api
 git config --global user.email "${GEMAIL}"
 git config --global user.name "${GUSER}"
-git clone https://github.com/"${GITHUB_REPOSITORY}" -b master master
-git clone https://github.com/"${GITHUB_REPOSITORY}" -b api api
+git clone https://github.com/DotOS/official_devices -b master master
+git clone https://github.com/DotOS/website_api -b master api
 python3 generate_api.py
 
 cd api
@@ -14,4 +14,4 @@ git add .
 CHANGED=$(git diff --numstat --cached | wc -l)
 [[ $CHANGED -eq 0 ]] && exit 0
 git commit -m "generate_api: $(date)"
-git push https://"${GUSER}":"${GPASSWORD}"@github.com/"${GITHUB_REPOSITORY}" api 
+git push https://"${GUSER}":"${GPASSWORD}"@github.com/DotOS/website_api master 
